445955a0b375319afe4435dfd255ceb1f77adaeb
Nahuel Lezcano <nlezcano.dev@gmail.com>
2023-03-25 19:31:47 -0300

    Remove StartUp - inicializacion de proyecto en Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,47 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Configuration;
 using TasksProject;
 using TasksProject.Context;
 
 var builder = WebApplication.CreateBuilder(args);
-new Startup(builder.Configuration).ConfigureServices(builder.Services);
+AppSettings.BindSettings(builder.Configuration);
 
-builder.Services.AddDbContext<AssignmentContext>(option => option.UseMySQL(AppSettings.Settings.ConnectionString));
 
+// Services container
+var services = builder.Services;
+services.AddControllers().AddNewtonsoftJson();
+services.AddHealthChecks();
+services.AddEndpointsApiExplorer();
+
+services.AddSwaggerGen(options =>
+{
+    var version = AppSettings.Settings.Version;
+
+    options.SwaggerDoc(version, new OpenApiInfo
+    {
+        Title = "Minimal task Api",
+        Version = version,
+        Description = "First Api in Dotnet using EF and learning about deep configuration in a WebApi project"
+    });
+});
+
+
+// Database configuration
+var connectionString = AppSettings.Settings.ConnectionString;
+services.AddDbContext<AssignmentContext>(option => option.UseMySQL(connectionString));
+
+
+// Dependency injection
 Bootstrapper.BootStrap();
+
+
+// BUILD
 var app = builder.Build();
 
+
+// Middlewares
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,16 +49,21 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors("tasks");
-app.MapControllers();
+app.UseRouting();
 
-app.MapGet("/", () => "HolaMundo");
-
-
-app.MapGet("/dbconnection", ([FromServices] AssignmentContext dbContext) =>
+// Controllers
+app.UseEndpoints(endpoints =>
 {
-    dbContext.Database.EnsureCreated();
-    return Results.Ok(dbContext.Database.IsRelational());
+    endpoints.MapControllers();
+    endpoints.MapHealthChecks("api/HealthCheck");
+
+    app.MapGet("/dbconnection", ([FromServices] AssignmentContext dbContext) =>
+    {
+        dbContext.Database.EnsureCreated();
+        return Results.Ok(dbContext.Database.IsRelational());
+    });
 });
 
 
+// RUN
 app.Run();
